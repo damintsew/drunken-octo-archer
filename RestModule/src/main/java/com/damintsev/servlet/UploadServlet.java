e36bf679a115cc0ee4281263bbc912cbb8e98883
das <damintsew@gmail.com>
2014-03-31 22:38:48 +0400

    недокомичего

---
+++ RestModule/src/main/java/com/damintsev/servlet/UploadServlet.java
@@ -0,0 +1,25 @@
+package com.damintsev.servlet;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * User: adamintsev
+ * Date: 27.01.14
+ */
+//@Controller
+//@RequestMapping(value = "upload")
+public class UploadServlet {
+
+
+//    @RequestMapping(value = "image", method = RequestMethod.GET)
+    public  ResponseEntity<String> processFile()  {
+            return new ResponseEntity<>("fuck", HttpStatus.OK);
+    }
+
+
+}
